952b1d96035224deee432391f39272456a6bd943
Jamily Oliveira <jamilysw@gmail.com>
2022-09-26 12:57:49 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// CALCULO MEDIA
+
+var bim1 = 3;
+var bim2 = 7;
+var bim3 = 2;
+var bim4 = 10;
+
+var mediaAluno = (bim1 + bim2 + bim3 + bim4) / 4;
+var mediaMinima = 7;
+
+if(mediaAluno >= mediaMinima){
+    console.log("Aluno foi aprovado!");
+} else {
+    console.log("Aluno foi reprovado!");
+}
+
+// CALCULO MEDIANA
+
+/*
+var medianaElemento1 = 4/2;
+var medianaElemento2 = (4/2) + 1;
+
+var mediaDasMedianas = (bim2 + bim3) / 2;
+
+if(mediaDasMedianas >= 7){
+    console.log("Aluno foi aprovado!")
+} else {
+    console.log("Aluno foi reprovado")
+}*/
